72ca031189494b04acdd62090ff0ff5f3f5b1c8b
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2018-11-27 18:05:41 +0100

    Added login and register views and set up their state, locally for now

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+
+class Login extends Component {
+
+  state = {
+    email: '',
+    password: ''
+  }
+
+  inputChangedHandler = e => this.setState({ [e.target.name]: e.target.value });
+
+  submitHandler = e => {
+    e.preventDefault();
+    const { email, password } = this.state;
+    const user = {
+      email,
+      password
+    }
+    console.log(user);
+  }
+
+  render() {
+    return (
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">Sign in to your DevConnector account</p>
+              <form onSubmit={this.submitHandler}>
+                <div className="form-group">
+                  <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" value={this.state.email} onChange={this.inputChangedHandler} />
+                </div>
+                <div className="form-group">
+                  <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" value={this.state.password} onChange={this.inputChangedHandler} />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" value="Submit" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Login;
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+
+class Register extends Component {
+
+  state = {
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+    errors: {}
+  }
+
+  inputChangedHandler = e => this.setState({ [e.target.name]: e.target.value });
+
+  submitHandler = e => {
+    e.preventDefault();
+    const { name, email, password, password2 } = this.state;
+    const newUser = {
+      name,
+      email,
+      password,
+      password2
+    }
+    console.log(newUser);
+  }
+
+  render() {
+    return (
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Sign Up</h1>
+              <p className="lead text-center">Create your DevConnector account</p>
+              <form onSubmit={this.submitHandler}>
+                <div className="form-group">
+                  <input type="text" className="form-control form-control-lg" placeholder="Name" name="name" value={this.state.name} onChange={this.inputChangedHandler} />
+                </div>
+                <div className="form-group">
+                  <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" value={this.state.email} onChange={this.inputChangedHandler} />
+                  <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+                </div>
+                <div className="form-group">
+                  <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" value={this.state.password} onChange={this.inputChangedHandler} />
+                </div>
+                <div className="form-group">
+                  <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="password2" value={this.state.password2} onChange={this.inputChangedHandler} />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" value="Submit" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Register;
